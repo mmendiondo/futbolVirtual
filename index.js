065328affde24c6d74533072fe0cc2077bcaf882
mmendiondo <mmendiondo@ondiss.com.ar>
2014-10-01 18:13:09 -0300

    added handlers for players

--- index.js
+++ index.js
@@ -2,29 +2,96 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-
-
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
+//["Dar Pase", "Patear al Arco", "Gambetear", "Atajar"];
+
+var match = {};
+match.positionsTeam1 = {
+							"1" : {"userName": "","position" : "goalKeeper", "isCurrent" : false},
+							"2" : {"userName": "","position" : "2", "isCurrent" : false},
+							"3" : {"userName": "","position" : "3", "isCurrent" : false},
+							"4" : {"userName": "","position" : "5", "isCurrent" : false},
+							"5" : {"userName": "","position" : "9", "isCurrent" : true}
+						};
+
+match.positionsTeam2 = {
+							"1" : {"userName": "","position" : "goalKeeper", "isCurrent" : false},
+							"2" : {"userName": "","position" : "2", "isCurrent" : false},
+							"3" : {"userName": "","position" : "3", "isCurrent" : false},
+							"4" : {"userName": "","position" : "5", "isCurrent" : false},
+							"5" : {"userName": "","position" : "9", "isCurrent" : false}
+						};
+
+var clients = [];
+
 io.on('connection', function(socket){
+
 	socket.on('chat message', function(msg){
 		io.emit('chat message', msg);
 	});
 
-	socket.on('game connected', function(msg){
-		//agregar usuario by id
-	    
-	  });
+	socket.on('connect game', function(msg){
+
+		var obj = JSON.parse(msg);
+		var _userName = obj.userName;
+		var foundTeam = false;
+		for(var player in match.positionsTeam1)
+		{
+			if(match.positionsTeam1[player].userName == ""){
+				match.positionsTeam1[player].userName = _userName;
+				obj.team = "1";
+				obj.position = match.positionsTeam2[player].position;
+				foundTeam = true;
+				break;
+			}
+		}
+		if(!foundTeam){
+			for(var player in match.positionsTeam2)
+			{
+				if(match.positionsTeam2[player].userName == ""){
+					match.positionsTeam2[player].userName = _userName;
+					obj.team = "2";
+					obj.position = match.positionsTeam2[player].position;
+					break;
+				}
+			}
+		}
+
+		clients.push(socket);
+		socket.emit('game connected', JSON.stringify(obj));
+	});
 
 	socket.on('game action', function(msg){
-
-		//calculate stuff and emit to current player now to take actions
-	    io.emit('game available actions', "actions as json");
-	  });
+		var actions = resolveGameAction(msg);
+		clients[0].emit('game available actions', actions);
+		//io.emit('game available actions', actions);
+	});
 
 });
+
+
+function resolveGameAction(message)
+{
+
+	//here resolve
+		var obj = JSON.parse(message);
+
+		console.log(obj);
+		var action = obj.action;
+		var userName = obj.userName;
+		var actions = [];
+
+		//notifications limit to 3at the same time
+		actions.push("Dar Pase");
+		actions.push("Patear al Arco");
+		actions.push("Gambetear");
+		//actions.push("Atajar");
+
+		return JSON.stringify(actions);
+}
 
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening *:');
