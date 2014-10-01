00194afc06131bd3c28b81eedff7a59500ae5b75
Matias Mendiondo <mmendiondo@ondiss.com.ar>
2014-10-01 00:37:57 -0300

    stuff

---
+++ README.md
@@ -0,0 +1,1 @@
+# futbol virtual notifications
---
+++ index.js
@@ -0,0 +1,31 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+
+	socket.on('game connected', function(msg){
+		//agregar usuario by id
+	    
+	  });
+
+	socket.on('game action', function(msg){
+
+		//calculate stuff and emit to current player now to take actions
+	    io.emit('game available actions', "actions as json");
+	  });
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
